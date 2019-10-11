f7149d79f985f9f721ac0b2bbf568be301ed4688
webox-de <info@webox-de.com>
2019-10-12 01:12:08 +0900

    JS練習ファイル作成

--- app/index.php
+++ app/index.php
@@ -1,5 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
 
-echo '<p>Hello Takahashi</p>';
-
-?>
+</body>
+</html>
