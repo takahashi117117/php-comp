7db40deb5f40160de62f49a153be98ba82d9063c
takahashi117117 <info@startkit.jp>
2019-10-18 18:48:25 +0900

    JS練習

--- app/js-works06/script.js
+++ app/js-works06/script.js
@@ -1,11 +1,18 @@
 $(function() {
-  $(".tab_btn li").click(function() {
-    let index = $(".tab_btn li").index(this);
-    $(".tab_btn li").removeClass("active");
-    $(this).addClass("active");
-    $(".tab_box")
-      .removeClass("show")
-      .eq(index)
-      .addClass("show");
+  const MSG_TEXT_MAX = "20文字以内で入力してください。";
+  const MSG_EMPTY = "入力必須です。";
+  const MSG_EMIL_TYPE = "emailの形式ではありません。";
+  const MSG_TEXTAREA_MAX = "100文字以内で入力してください。";
+
+  $(".valid-text").keyup(function() {
+    let form_g = $(this).closest(".form-group");
+
+    if ($(this).val().length > 20) {
+      form_g.removeClass("has-success").addClass("has-error");
+      form_g.find(".help-block").text(MSG_TEXT_MAX);
+    } else {
+      form_g.removeClass("has-error").addClass("has-success");
+      form_g.find(".help-block").text("");
+    }
   });
 });
